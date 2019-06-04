292914233dbfe7bd17a8ed0391eea1c8c55ba0cb
Sally <46608728+SRAdda@users.noreply.github.com>
2019-06-04 09:21:02 -0700

    adds news.php file and template for subcategories

---
+++ feedstemplate.php
@@ -0,0 +1,113 @@
+
+	
+     <!DOCTYPE html>
+<html lang="en">
+  <head>
+		<!-- start of meta include file -->
+	<title>News</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta name="description" content="News - Welcome to the Cosmos Power Survey website.  Make the most of it." />
+	<meta name="keywords" content="Cosmos,Survey,database,mysql,php" />
+	<meta name="robots" content="no index, no follow" />
+	<meta http-equiv="Cache-Control" content="no-cache" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="-1" />
+	<script language="JavaScript" type="text/javascript">
+		<!-- This JS disallows hijacking into someone else's frame...
+		 if (top.location != self.location){top.location=self.location}
+		//-->
+	</script>
+		<!-- end of meta include file -->    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--Bootstrap themes use style settings to change look and feel -->
+    <link rel="stylesheet" href="http://sallyra.dreamhosters.com/itc250-sp19/themes/Bootswatch/css/cosmo.css" media="screen">
+    <link rel="stylesheet" href="http://sallyra.dreamhosters.com/itc250-sp19/themes/Bootswatch/css/bootswatch.min.css">
+	<link rel="stylesheet" href="http://sallyra.dreamhosters.com/itc250-sp19/themes/Bootswatch/css/bootswatch-overrides.css">
+    <!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+     <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="http://sallyra.dreamhosters.com/itc250-sp19/" class="navbar-brand">Cosmos Power Survey</a>
+		    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+			<li><a href="http://sallyra.dreamhosters.com/itc250-sp19/index.php" title="A model for building largely static web pages">Home</a></li>
+<li><a href="http://sallyra.dreamhosters.com/itc250-sp19/surveys/" title="The entrance to our Survey App">Surveys</a></li>
+<li><a href="http://sallyra.dreamhosters.com/itc250-sp19/feeds/feed.php/" title="Seattle Lifestyle">News</a></li>
+<li><a href="http://sallyra.dreamhosters.com/itc250-sp19/demo/demo_shared.php" title="A demo page for building mysqli shared connection based applications.">Shared</a></li>
+<li><a href="http://sallyra.dreamhosters.com/itc250-sp19/demo/demo_pdo.php" title="A demo page for building PDO connection based applications.">PDO</a></li>
+<li><a href="http://sallyra.dreamhosters.com/itc250-sp19/demo/demo_contact.php" title="A demo for building postback forms">Contact</a></li>
+          </ul>
+			<ul class="nav navbar-nav navbar-right"><li><a href="http://sallyra.dreamhosters.com/itc250-sp19/admin/admin_login.php" title="Login to site">Login</a></li>
+</ul>        </div>
+      </div>
+    </div>
+    <div class="container">
+				
+<h3 align="center">News</h3>
+
+<html>
+<div class="container">
+<h3 align="center">Seattle Lifestyle</h3>
+
+
+<div align="center">Seattle Entertainment and News</div>
+
+<table class="table table-hover">
+<thead>
+<tr>
+<th scope="col">Categories</th>
+</tr>
+</thead>
+<tbody>
+
+ <tr>
+<td><a href="sallyra.dreamhosters.com/itc250-sp19/feeds/subcategories.php?id=1">Movie News and Listings</a></td>
+</tr>
+
+ <tr>
+<td><a href="http://sallyra.dreamhosters.com/itc250-sp19/feeds/subcategories.php?id=2">Live Theatre Around Seattle</a></td>
+</tr>
+
+ <tr>
+<td><a href="http://sallyra.dreamhosters.com/itc250-sp19/feeds/subcategories.php?id=3">Sports News and Events</a></td>
+</tr>
+
+
+
+</tbody>
+</table>
+</div>
+</div>
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+            
+            <ul class="list-unstyled">
+              <li class="pull-right"><a href="#top">Back to top</a></li>
+            </ul>
+			<p>Cosmos Power, &copy; 2014 - 2019. Theme by <a href="http://bootswatch.com/">Bootswatch</a>, based on <a href="http://getbootstrap.com/css/">Bootstrap</a>.
+			</p>
+
+          </div>
+        </div>
+        
+      </footer>
+    
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="http://sallyra.dreamhosters.com/itc250-sp19/themes/Bootswatch/js/bootstrap.min.js"></script>
+    <script src="http://sallyra.dreamhosters.com/itc250-sp19/themes/Bootswatch/js/bootswatch.js"></script>
+  </body>
+</html>
