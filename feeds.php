d37af0d23767b4b795dd3d3a2799699dd41ed881
cagunning <46728544+cagunning@users.noreply.github.com>
2019-06-04 22:16:33 -0700

    Create feeds.php
    current version of feeds file. but only links to one feed vial a url of that. i

---
+++ feeds.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * demo_list_pager.php along with demo_view_pager.php provides a sample web application
+ *
+ * The difference between demo_list.php and demo_list_pager.php is the reference to the 
+ * Pager class which processes a mysqli SQL statement and spans records across multiple  
+ * pages. 
+ *
+ * The associated view page, demo_view_pager.php is virtually identical to demo_view.php. 
+ * The only difference is the pager version links to the list pager version to create a 
+ * separate application from the original list/view. 
+ * 
+ * @package nmPager
+ * @author Bill Newman <williamnewman@gmail.com>
+ * @version 3.02 2011/05/18
+ * @link http://www.newmanix.com/
+ * @license https://www.apache.org/licenses/LICENSE-2.0
+ * @see demo_view_pager.php
+ * @see Pager.php 
+ * @todo none
+ */
+
+# '../' works for a sub-folder.  use './' for the root  
+require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials 
+ 
+# SQL statement
+$sql = "select FeedName, FeedID, FeedUrl from sp19_feeds";
+
+#Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
+$config->titleTag = 'P4 News Feeds';
+
+#Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
+$config->metaDescription = 'Seattle Central\'s ITC280 Class Muffins are made with pure PHP! ' . $config->metaDescription;
+$config->metaKeywords = 'Feeds,PHP,Fun,Bran,Regular,Regular Expressions,'. $config->metaKeywords;
+
+//adds font awesome icons for arrows on pager
+$config->loadhead .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
+
+/*
+$config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
+$config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
+$config->metaRobots = 'no index, no follow';
+$config->loadhead = ''; #load page specific JS
+$config->banner = ''; #goes inside header
+$config->copyright = ''; #goes inside footer
+$config->sidebar1 = ''; #goes inside left side of page
+$config->sidebar2 = ''; #goes inside right side of page
+$config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
+$config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
+*/
+
+# END CONFIG AREA ---------------------------------------------------------- 
+
+
+
+
+get_header(); #defaults to theme header or header_inc.php
+?>
+
+<h3 align="center">News Categories Page</h3>
+
+<p>This is our list page of News feed categories</p> 
+<?php
+#reference images for pager
+//$prev = '<img src="' . $config->virtual_path . '/images/arrow_prev.gif" border="0" />';
+//$next = '<img src="' . $config->virtual_path . '/images/arrow_next.gif" border="0" />';
+
+#images in this case are from font awesome
+$prev = '<i class="fa fa-chevron-circle-left"></i>';
+$next = '<i class="fa fa-chevron-circle-right"></i>';
+
+# Create instance of new 'pager' class
+$myPager = new Pager(2,'',$prev,$next,'');
+$sql = $myPager->loadSQL($sql);  #load SQL, add offset
+
+# connection comes first in mysqli (improved) function
+// $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+// if(mysqli_num_rows($result) > 0)
+// {#records exist - process
+// 	if($myPager->showTotal()==1){$itemz = "feed";}else{$itemz = "feeds";}  //deal with plural
+//     echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
+// 	while($row = mysqli_fetch_assoc($result))
+// 	{# process each row
+//          echo '<div align="center"><a href="' . VIRTUAL_PATH . 'news/feeds.php?id=' . (int)$row['FeedID'] . '">' . dbOut($row['FeedName']) . '</a>';
+// 	}
+// 	echo $myPager->showNAV(); # show paging nav, only if enough records	 
+// }else{#no records
+//     echo "<div align=center>What! No feeds?  There must be a mistake!!</div>";	
+// }
+// @mysqli_free_result($result);
+
+
+//read-feed-simpleXML.php
+//our simplest example of consuming an RSS feed
+
+  $feedUrl = "https://news.google.com/rss/search?q=seattle+dance&hl=en-US&gl=US&ceid=US:en";
+  $feedUrl = file_get_contents($feedUrl);
+  $xml = simplexml_load_string($feedUrl);
+  print '<h1>' . $xml->channel->title . '</h1>';
+  foreach($xml->channel->item as $story)
+  {
+    echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
+    echo '<p>' . $story->description . '</p><br /><br />';
+  }
+
+get_footer(); #defaults to theme footer or footer_inc.php   
+?>
